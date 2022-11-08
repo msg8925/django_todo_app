1715faf9a2a8af0ad50f230c680660822598d4cc
msg8925 <msg.8925@gmail.com>
2022-11-08 14:54:33 +0700

    Login/logout completed

--- todoapp/users/views.py
+++ todoapp/users/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
 
-    form = UserRegistrationForm()
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Profile successfully created.')
+            return redirect('todo-home')
 
-    context = {
-        "form": form
-    }
 
-    return render(request, 'users/register.html', context)
+    else:
+        form = UserRegistrationForm()
+        
+        # context = {
+        #     "form": form
+        # }
+
+    return render(request, 'users/register.html', {"form": form})
 
