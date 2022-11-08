3194c19df08cffd20ce1ac3c82a45f6a7ae5c8f9
msg8925 <msg.8925@gmail.com>
2022-11-08 12:07:21 +0700

    Task model added

--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.BinaryField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
