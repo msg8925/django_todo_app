15e4dc1fa2cb87d6bbf0205d1ae293ef1d13339e
msg8925 <msg.8925@gmail.com>
2022-11-15 12:34:19 +0700

    Profile link added to navbar

--- todoapp/users/views.py
+++ todoapp/users/views.py
@@ -27,12 +27,26 @@ def register(request):
 @login_required
 def profile(request):
 
-    p_form = ProfileUpdateForm(instance=request.user.profile)
-    u_form = UserUpdateForm(instance=request.user)
+    if request.method == 'POST':
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+          
+        if p_form.is_valid and u_form.is_valid:
+            p_form.save()
+            u_form.save()  
 
-    context = {
-        "p_form": p_form,
-        "u_form": u_form
-    }
+            messages.success(request, "Profile successfully updated.")
+
+            return redirect("profile")
+
+    else:
+
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        u_form = UserUpdateForm(instance=request.user)
+
+        context = {
+            "p_form": p_form,
+            "u_form": u_form
+        }
 
     return render(request, "users/profile.html", context)
