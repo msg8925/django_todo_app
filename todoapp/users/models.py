68b6d17f9c89016f4dbba27d3280821351a63fec
msg8925 <msg.8925@gmail.com>
2022-11-15 13:35:36 +0700

    Adding signals

--- todoapp/users/models.py
+++ todoapp/users/models.py
@@ -7,4 +7,12 @@ class Profile(models.Model):
     image = models.ImageField(default="default.jpg", upload_to="profile_pics")
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def save(self):
+        super().save()
 
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
